96d1d0bd2f9ee2d79dfacd0b67f3c71a997b3778
Kanish Gosain <regokan@gmail.com>
2020-06-24 11:59:41 +0530

    Added routes to Express app

--- layer7-reverse-proxy/express-app/index.js
+++ layer7-reverse-proxy/express-app/index.js
@@ -4,5 +4,11 @@ const port = 3000;
 
 const serverName = process.env.SERVER_NAME;
 
-app.get("/", (req, res) => res.send(`Responding from APP ${serverName}`));
+app.get("/", (req, res) => res.send(`Responding from App ${serverName}`));
+app.get("/route1", (req, res) =>
+  res.send(`Responding from Route 1 of App ${serverName}`)
+);
+app.get("/route2", (req, res) =>
+  res.send(`Responding from Route 2 of App ${serverName}`)
+);
 app.listen(port);
